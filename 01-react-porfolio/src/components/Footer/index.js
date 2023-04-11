e8bbfdd8ca684040b896ed0cc8ad1daf45b3e9c0
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2023-04-11 17:21:59 -0500

    footer icon

--- 01-react-porfolio/src/components/Footer/index.js
+++ 01-react-porfolio/src/components/Footer/index.js
@@ -1,6 +1,6 @@
 
 import React from "react"
-// import GitHublogo from "../../images/GitHublogo.png";
+import GitHublogo from "../../Assets/photos/footer/GitHublogo.png";
 
 
 const Footer = () => {
@@ -12,16 +12,17 @@ const Footer = () => {
 
                     <hr className="clearfix w-100 d-md-none pb-0" />
 
-                    <div className="col-md-3 mb-md-0 mb-3">
+                    <div className="footer">
                         <ul className="list-unstyled">
-                            <li><a href="https://github.com/christian1512-FE">Github</a></li>
-                            {/* <img className="icon" src ={GitHublogo} alt="github logo"></img> */}
+                            <a href="https://github.com/christian1512-FE"><img className="icon" src ={GitHublogo} alt="GitHubicon" /></a>
                         </ul>
                     </div>
                 </div>
             </div>
+            <section className="footercontainer">
             <div className="footer-copyright text-center py-3">© {new Date().getFullYear()} Copyright: Christian Mendez
             </div>
+            </section>
         </footer>
     );
 };
